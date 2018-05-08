b44e6870e7baaa09498f814465744df021b2e503
fengwj33 <fengwj33@hotmail.com>
2018-05-08 20:26:03 +0800

    AddStudent

---
+++ SDN/static/Student.js
@@ -0,0 +1,70 @@
+function getlist()
+{
+    
+    $.ajax({
+        url: "/GetStudentList",
+        type: "GET",
+        dataType: "json",
+        success: function(data) {
+            var obj=document.getElementById("Tlist");
+            var str=""
+            var uname=""
+            $.each(data.body, function(i, item) {
+                str+="<tr>" 
+                $.each(item, function(j, element) {
+                    if(j==1)
+                        uname=element;
+                    str+="<td>"+element+"</td>"
+                })
+                str+="<td><button type=\"button\" class=\"btn btn-danger btn-xs\" onclick=\"dellist('"+uname+"')\"  >删除</button></td></tr>"
+            })
+            obj.innerHTML=str
+        }
+    })
+}
+function addstudent()
+{
+    var UserName=document.getElementById("inputUser").value;
+    var StudentName=document.getElementById("inputSName").value;
+    var Password=document.getElementById("inputPassword").value;
+    //alert(TeacherName)
+    $.ajax({
+        type: "post",
+        url: "/AddStudent",
+        data: {
+            "UserName": UserName,
+            "StudentName": StudentName,
+            "Password": Password,
+        },
+        dataType: 'text',
+        success: function (data) {
+            //alert("Success");
+            getlist("Tlist");
+        },
+        error: function () {
+            //alert("error");
+        }
+    })
+    
+}
+function dellist(id)
+{
+    alert(id);
+    return;
+    $.ajax({
+        type: "post",
+        url: "/removeTeacher",
+        data: {
+            "UserName": id
+        },
+        dataType: 'text',
+        success: function (data) {
+            alert("remove:"+id);
+            getlist()
+        },
+        error: function () {
+            alert("error");
+        }
+    })
+    
+}
